280528336e86cc73bef414f4683e544bd9cf80ab
Lewis Sardena <lewis.sardena@me.com>
2019-10-21 11:29:29 +0100

    added data sources

--- main.tf
+++ main.tf
@@ -2,10 +2,18 @@ provider "aws" {
     region = "eu-west-1"
 }
 
-resource "aws_instance" "example" {
-    ami                    = "ami-0c55b159cbfafe1f0"
-    instance_type          = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnet_ids" "default" {
+    vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_launch_configuration" "example" {
+    image_id       = "ami-0c55b159cbfafe1f0"
+    instance_type  = "t2.micro"
+    security_groups = [aws_security_group.instance.id]
 
     user_data = <<-EOF
                 #!/bin/bash
@@ -13,8 +21,22 @@ resource "aws_instance" "example" {
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
-    tags = {
-        Name = "terraform-example"
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key                 = "Name"
+        value               = "terraform-asg-example"
+        propagate_at_launch = true
     }
 }
 
@@ -33,9 +55,4 @@ variable "server_port" {
     description = "The port the server will use for HTTP requests"
     type        = number
     default     = 8080
-}
-
-output "public_ip" {
-    value = aws_instance.example.public_ip
-    description = "The public ip address of the web server"
 }
